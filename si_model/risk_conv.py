f625bb93708562d9b20b732b1a13d8f44adff9d8
Jan Schering <j.schering@web.de>
2022-11-04 21:32:56 +0100

    pytesting

--- si_model/risk_conv.py
+++ si_model/risk_conv.py
@@ -20,37 +20,3 @@ def risk_convolution2D(batch: t.Tensor):
     transposed = t.permute(expanded, (0, 3, 1, 2))
 
     return conv2d(transposed, kernel, stride=1, padding="valid")
-
-
-if __name__ == "__main__":
-    print("testing the risk convolution function...")
-    size = 9
-    batch_size = 2
-    beta = 0.35
-
-    batch = t.zeros((batch_size, size, size))
-    batch[:, size // 2, size // 2] = 1
-
-    batch[0, 0, 0] = 1
-    batch[0, 0, 1] = 1
-    batch[0, 0, 2] = 1
-    batch[0, 1, 0] = 1
-    batch[0, 1, 2] = 1
-    batch[0, 2, 0] = 1
-    batch[0, 2, 1] = 1
-    batch[0, 2, 2] = 1
-
-    conv_result = risk_convolution2D(batch)
-
-    assert conv_result[0, 0, 0, 0] == 2
-    assert conv_result[0, 0, 0, 1] == 4
-    assert conv_result[0, 0, 0, 2] == 2
-    assert conv_result[0, 0, 1, 0] == 4
-    assert conv_result[0, 0, 1, 1] == 8
-    assert conv_result[0, 0, 1, 2] == 4
-    assert conv_result[0, 0, 2, 0] == 2
-    assert conv_result[0, 0, -1, 1] == 3
-    assert conv_result[0, 0, -1, 0] == 2
-    assert conv_result[0, 0, -1, 2] == 2
-
-    print("all tests passed successfully")
---
+++ si_model/test_risk_conv.py
@@ -0,0 +1,32 @@
+import torch as t
+from risk_conv import risk_convolution2D
+
+
+def test_risk_conv1():
+    size = 9
+    batch_size = 2
+
+    batch = t.zeros((batch_size, size, size))
+    batch[:, size // 2, size // 2] = 1
+
+    batch[0, 0, 0] = 1
+    batch[0, 0, 1] = 1
+    batch[0, 0, 2] = 1
+    batch[0, 1, 0] = 1
+    batch[0, 1, 2] = 1
+    batch[0, 2, 0] = 1
+    batch[0, 2, 1] = 1
+    batch[0, 2, 2] = 1
+
+    conv_result = risk_convolution2D(batch)
+
+    assert conv_result[0, 0, 0, 0] == 2
+    assert conv_result[0, 0, 0, 1] == 4
+    assert conv_result[0, 0, 0, 2] == 2
+    assert conv_result[0, 0, 1, 0] == 4
+    assert conv_result[0, 0, 1, 1] == 8
+    assert conv_result[0, 0, 1, 2] == 4
+    assert conv_result[0, 0, 2, 0] == 2
+    assert conv_result[0, 0, -1, 1] == 3
+    assert conv_result[0, 0, -1, 0] == 2
+    assert conv_result[0, 0, -1, 2] == 2
