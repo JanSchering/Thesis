30b313709c42dec6df13019fcf5466b536cbff99
Jan Schering <j.schering@web.de>
2022-11-15 12:44:04 +0100

    adhesion for CPM

--- cpm/adhesion.py
+++ cpm/adhesion.py
@@ -1,4 +1,4 @@
-# %%
+from typing import List
 import torch as t
 import sys
 
@@ -6,9 +6,29 @@ sys.path.insert(0, "../si_model")
 from risk_conv import risk_convolution2D
 
 
-def adhesion(grid: t.Tensor, cell_id: t.Tensor):
-    cell_mask = (grid != cell_id).double()
-    active_neighbors = risk_convolution2D(cell_mask)
-    valid_neighbors = active_neighbors * (1 - cell_mask)
+def contact_points(grid: t.Tensor, cell_id: int, target_id: int) -> t.Tensor:
+    """
+    Find the number of contact points on the 2D lattice <grid> between <cell_id>
+    and <target_id>.
+    """
+    cell_mask = (grid == cell_id).double()
+    target_mask = (grid == target_id).double()
+    active_neighbors = risk_convolution2D(target_mask)
+    valid_neighbors = active_neighbors * cell_mask
 
     return t.sum(valid_neighbors, dim=(-1, -2))
+
+
+def adhesion(grid: t.Tensor, cell_id: int, penalties: dict) -> t.Tensor:
+    """
+    Calculate the adhesion of cell <cell_id> on the grid given some <penalties>.
+
+    grid:       The CPM lattice.
+    cell_id:    The ID of the cell to calculate the adhesion energy for.
+    penalties:  penalty J for each interface between <cell_id> and other cell IDs.
+    """
+    total_adhesion = 0
+    for target_id in penalties:
+        penalty = penalties[target_id]
+        total_adhesion += contact_points(grid, cell_id, target_id) * penalty
+    return total_adhesion
--- cpm/test/test_adhesion.py
+++ cpm/test/test_adhesion.py
@@ -2,10 +2,10 @@ import sys
 
 sys.path.insert(0, "../")
 import torch as t
-from adhesion import adhesion
+from adhesion import contact_points, adhesion
 
 
-def test_adhesion_1():
+def init_case_1():
     """
     [   [0, 0, 0, 0, 0]
         [0, 0, 1, 0, 0]
@@ -19,12 +19,10 @@ def test_adhesion_1():
     grid[0, 2, 3] = 1
     grid[0, 1, 2] = 1
     grid[0, 3, 2] = 1
-
-    assert adhesion(grid, 1) == 24
-    assert adhesion(grid, 0) == 24
+    return grid
 
 
-def test_adhesion_2():
+def init_case_2():
     """
     [   [0, 0, 0, 0, 0]
         [0, 2, 1, 1, 0]
@@ -43,13 +41,10 @@ def test_adhesion_2():
     grid[0, 3, 1] = 2
     grid[0, 3, 2] = 1
     grid[0, 3, 3] = 1
-
-    assert adhesion(grid, 1) == 28
-    assert adhesion(grid, 2) == 20
-    assert adhesion(grid, 0) == 34
+    return grid
 
 
-def test_adhesion_3():
+def init_case_3():
     """
     [   [0, 3, 0, 3, 0]
         [0, 2, 1, 1, 0]
@@ -72,8 +67,53 @@ def test_adhesion_3():
     grid[0, 3, 3] = 1
     grid[0, 4, 0] = 1
     grid[0, 4, 2] = 1
+    return grid
 
-    assert adhesion(grid, 1) == 40
-    assert adhesion(grid, 3) == 16
-    assert adhesion(grid, 2) == 20
-    assert adhesion(grid, 0) == 44
+
+def test_contact_points_1():
+    grid = init_case_1()
+    assert contact_points(grid, 1, 0) == 24
+
+
+def test_adhesion_1():
+    grid = init_case_1()
+    penalties = {0: 25}
+    assert adhesion(grid, 1, penalties) == 24 * 25
+
+
+def test_contact_points_2():
+    grid = init_case_2()
+    assert contact_points(grid, 1, 2) == 7
+    assert contact_points(grid, 1, 0) == 21
+    assert contact_points(grid, 2, 0) == 13
+
+
+def test_adhesion_2():
+    grid = init_case_2()
+    penalties_1 = {0: 34, 2: 56}
+    penalties_2 = {0: 20, 1: 56}
+
+    assert adhesion(grid, cell_id=1, penalties=penalties_1) == 34 * 21 + 56 * 7
+    assert adhesion(grid, cell_id=2, penalties=penalties_2) == 20 * 13 + 56 * 7
+
+
+def test_contact_points_3():
+    grid = init_case_3()
+
+    assert contact_points(grid, 1, 2) == 9
+    assert contact_points(grid, 1, 3) == 6
+    assert contact_points(grid, 2, 3) == 1
+    assert contact_points(grid, 1, 0) == 25
+    assert contact_points(grid, 2, 0) == 10
+    assert contact_points(grid, 3, 0) == 9
+
+
+def test_adhesion_3():
+    grid = init_case_3()
+    penalties_1 = {0: 10, 2: 20, 3: 30}
+    penalties_2 = {0: 40, 1: 50, 3: 60}
+    penalties_3 = {0: 70, 1: 80, 2: 90}
+
+    assert adhesion(grid, cell_id=1, penalties=penalties_1) == 25 * 10 + 9 * 20 + 6 * 30
+    assert adhesion(grid, cell_id=2, penalties=penalties_2) == 10 * 40 + 9 * 50 + 1 * 60
+    assert adhesion(grid, cell_id=3, penalties=penalties_3) == 9 * 70 + 6 * 80 + 1 * 90
