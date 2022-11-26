2a5299712256ba88e1f2472b3724c4df3d54e818
Lili Pogoda <estherphilinedorsch@googlemail.com>
2022-11-26 21:34:26 +0100

    current

--- fhn_model/reaction.py
+++ fhn_model/reaction.py
@@ -52,18 +52,25 @@ def sigma(
     probability_func: Callable,
 ):
     num_cells = cells.shape[-1]
+    # use the probability function to get the reaction prob. for each of the cells
     reaction_probs = probability_func(cells, N, gamma, rate_coefficient)
+    # randomly sample a threshold value for each cell to compare the prob. against
     thresholds = uniform.Uniform(0, 1).sample_n(num_cells)
+    # edge values to use for the heaviside at x=0
     edge_vals = t.zeros(num_cells)
     if cells.is_cuda:
         thresholds = thresholds.cuda()
         edge_vals = edge_vals.cuda()
+    # for channel 0 and 2, the number of particles for species A should increase according to the prob.
     if channel == 0 or channel == 2:
         cells[0] += t.heaviside(reaction_probs - thresholds, values=edge_vals)
+    # for channel 1 and 3, the number of particles for species A should decrease according to the prob.
     elif channel == 1 or channel == 3:
         cells[0] -= t.heaviside(reaction_probs - thresholds, values=edge_vals)
+    # for channel 4, the number of particles for species B should increase according to the prob.
     elif channel == 4:
         cells[1] += t.heaviside(reaction_probs - thresholds, values=edge_vals)
+    # for channel 5, the number of particles for species B should decrease according to the prob.
     elif channel == 5:
         cells[1] -= t.heaviside(reaction_probs - thresholds, values=edge_vals)
     return cells
@@ -78,14 +85,20 @@ def rho(
     num_reaction_channels: int,
 ):
     grid_size = grid.shape[-1]
+    # for each cell on the lattice, choose a reaction channel
     channels = t.randint_like(t.zeros(grid_size, grid_size), high=num_reaction_channels)
     if grid.is_cuda:
         channels = channels.cuda()
+    # iterate over each reaction channel 
     for channel_idx in range(num_reaction_channels):
+        # get the reaction probability function of the current channel
         p_func = probability_funcs[channel_idx]
+        # get the rate coefficient of the current channel 
         rate_coefficient = rate_coefficients[channel_idx]
+        # mask out all cells that use a different reaction channel
         channel_mask = channels == channel_idx
         cells = grid[:, channel_mask].detach().clone()
+        # run the local reaction operator on each cell and update their state 
         grid[:, channel_mask] = sigma(
             cells, channel_idx, N, gamma, rate_coefficient, p_func
         )
